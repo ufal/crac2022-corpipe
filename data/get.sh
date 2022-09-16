b6827809f1a9ae6a82addb5125f640bc15f86c94
Milan Straka <milan@strakovi.com>
2022-09-16 06:55:40 +0200

    Add MPL-2 notice to all sources.

--- data/get.sh
+++ data/get.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# This file is part of CorPipe <https://github.com/ufal/crac2022-corpipe>.
+#
+# Copyright 2022 Institute of Formal and Applied Linguistics, Faculty of
+# Mathematics and Physics, Charles University in Prague, Czech Republic.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # Train and dev
 wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-4698/CorefUD-1.0-public.zip
 unzip CorefUD-1.0-public.zip
